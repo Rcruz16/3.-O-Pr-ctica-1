d847731283d60ff362ff11787ce26144bdd63bf8
Rcruz16 <ricardo.uriel1973@gmail.com>
2023-09-19 12:55:01 -0600

    HOLA

--- 3.-O Práctica 1/Form1.Designer.cs
+++ 3.-O Práctica 1/Form1.Designer.cs
@@ -29,12 +29,92 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_Ftc = new System.Windows.Forms.Button();
+            this.btn_Ctf = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(181, 164);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(447, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btn_Ftc
+            // 
+            this.btn_Ftc.Location = new System.Drawing.Point(318, 106);
+            this.btn_Ftc.Name = "btn_Ftc";
+            this.btn_Ftc.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ftc.TabIndex = 3;
+            this.btn_Ftc.Text = "F to C";
+            this.btn_Ftc.UseVisualStyleBackColor = true;
+            this.btn_Ftc.Click += new System.EventHandler(this.btn_Ftc_Click);
+            // 
+            // btn_Ctf
+            // 
+            this.btn_Ctf.Location = new System.Drawing.Point(318, 153);
+            this.btn_Ctf.Name = "btn_Ctf";
+            this.btn_Ctf.Size = new System.Drawing.Size(75, 23);
+            this.btn_Ctf.TabIndex = 4;
+            this.btn_Ctf.Text = "C to F";
+            this.btn_Ctf.UseVisualStyleBackColor = true;
+            this.btn_Ctf.Click += new System.EventHandler(this.btn_Ctf_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(318, 207);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_Clear.TabIndex = 5;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1258, 554);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Ctf);
+            this.Controls.Add(this.btn_Ftc);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.SystemColors.AppWorkspace;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_Ftc;
+        private System.Windows.Forms.Button btn_Ctf;
+        private System.Windows.Forms.Button btn_Clear;
     }
 }
 
--- 3.-O Práctica 1/Form1.cs
+++ 3.-O Práctica 1/Form1.cs
@@ -16,5 +16,26 @@ namespace _3._O_Práctica_1
         {
             InitializeComponent();
         }
+
+        private void btn_Ftc_Click(object sender, EventArgs e)
+        {
+            float F = float.Parse(textBox1.Text);
+            float C = (F - 32) * 5.0f / 9.0f;
+            textBox2.Text = C.ToString();
+
+        }
+
+        private void btn_Ctf_Click(object sender, EventArgs e)
+        {
+            float C = float.Parse(textBox2.Text);
+            float F = (C * 9f / 5f) + 32;
+            textBox1.Text = F.ToString();
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "o.o";
+            textBox2.Text = "o.o";
+        }
     }
 }
